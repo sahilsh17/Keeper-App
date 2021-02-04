7eb9aae57e74da021c9c4c8c1be497c752230569
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-03 23:23:21 -0800

    added App,Header and Footer components

---
+++ keeper-app/src/components/App.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import Header from './Header';
+import Footer from './Footer';
+export default function App() {
+  return(
+    <div>
+     <Header /><Footer/>
+    </div>
+  )
+}
---
+++ keeper-app/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import react from 'react';
+export default function Header() {
+  return(
+    <header> <h1>Keeper</h1></header>
+  )
+}
