ee57706e2cdb1ecb55818a94111e80a7a42da5a3
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-03 23:33:21 -0800

    added Note component

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function Note() {
+  return(
+    <div className="note">
+    <h1>This is the title</h1>
+    <p>This is the content</p>
+    </div>
+  )
+}
