e30c99318db0871e16dca27d64b4f170e7177d75
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-03 23:23:50 -0800

    Rendered App component

--- keeper-app/src/index.js
+++ keeper-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from "./components/App";
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('root')
 );
 //1. Create a new React app.
