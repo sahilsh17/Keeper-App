8780b8a8cbbba19831dae3748e98ef8990607153
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-06 15:59:43 -0800

    map function used to display all notes

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,12 +2,21 @@ import react from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import {notes} from '../notes.js';
 export default function App() {
   return(
     <div>
-     <Header />
-      <Note />
-     <Footer/>
-    </div>
+    <Header />
+    {
+  notes.map(note => {
+   return( <Note key={note.key} 
+          title={note.title} 
+          content={note.content}/>
+   )
+  })
+    }
+    <Footer/>
+  </div>
   )
+  
 }
