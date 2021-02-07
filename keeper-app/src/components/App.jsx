391dea45279a0fae37661a0283add8c8dbe3e379
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-06 17:11:07 -0800

    imported useState and created a notes state also sent it to CreateArea.jsx

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,21 +1,22 @@
-import react from 'react';
+import react, {useState} from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import {notes} from '../notes.js';
+import CreateArea from './CreateArea';
+
 export default function App() {
+  const [notes, setNotes] = useState([]);
   return(
     <div>
-    <Header />
-    {
-  notes.map(note => {
-   return( <Note key={note.key} 
-          title={note.title} 
-          content={note.content}/>
-   )
-  })
-    }
-    <Footer/>
+     <Header />
+      <CreateArea notes={notes} setNotes={setNotes}/>
+        {
+        notes.map((note,i) => {
+          return( <Note key={i} title={note.title} content={note.content} />)
+        })
+        }
+      <Footer />
   </div>
   )
   
