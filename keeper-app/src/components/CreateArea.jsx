f3f505952f7411821c8de8a48d007ebc746eb134
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-06 17:12:12 -0800

    added 2 functions=>changeHandler for changing note state and handleClick for changing notes state

--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-
-function CreateArea() {
+import React, {useState} from "react";
+function CreateArea(props) {
+  const [note, setNote] = useState({title: "", content: ""});
+  function changeHandler(e) {
+    setNote({...note,[e.target.name]: e.target.value});
+  }
+  function handleClick(e) {
+   e.preventDefault();
+   console.log(props.notes);
+    let temp = [...props.notes];
+    props.setNotes([...temp,note]);
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" onChange={changeHandler}/>
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={changeHandler} />
+        <button onClick={handleClick}>Add</button>
       </form>
     </div>
   );
