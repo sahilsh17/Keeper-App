822f69fb88e8f4678758ae0b4523fe15e71ef46c
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-07 21:57:15 -0800

    imported Zoom from material-ui for adding zoom in effect for add button

--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
+import { Zoom } from '@material-ui/core';
 
 function CreateArea(props) {
   const [note, setNote] = useState({title: "", content: ""});
@@ -19,7 +20,9 @@ function CreateArea(props) {
       <form className="create-note">
         <input name="title" value={note.title} placeholder="Title" onChange={changeHandler}/>
         <textarea name="content" value={note.content} placeholder="Take a note..." rows="3" onChange={changeHandler} />
+        <Zoom in={true}>
         <Fab onClick={handleClick}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
