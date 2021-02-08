2530f120054ab2d6c3cd50c12ec50e86aee4d4da
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-07 20:57:22 -0800

    created handleClick for deleting note

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -1,10 +1,15 @@
 import react from 'react';
 
 export default function Note(props) {
+  function handleClick(e) {
+    e.preventDefault();
+    props.onDelete(props.id);
+  }
   return(
     <div className="note">
     <h1>{props.title}</h1>
     <p>{props.content}</p>
+    <button onClick={handleClick}>DELETE</button>
     </div>
   )
 }
