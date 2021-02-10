a4e2061784ed3013f067abb3e71ed4b07b7829ef
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-09 20:48:47 -0800

    added a new route to notes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,36 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-app.use('/',(req, res) => {
-console.log(req.headers);
-res.send('Hello World!');
-});
-app.listen(port, function(){
-console.log(`Server is running at ${port}`);
-});
+const PORT       = process.env.PORT || 8000;
+const ENV        = process.env.ENV || "development";
+const express    = require("express");
+const bodyParser = require("body-parser");
+
+require('dotenv').config();
+const app        = express();
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+// Separated Routes for each Resource
+// Note: Feel free to replace the example routes below with your own
+const noteRoutes = require("./routes/notes");
+
+// Mount all resource routes
+// Note: Feel free to replace the example routes below with your own
+ app.use("/api/notes", noteRoutes(db));
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+}); 
