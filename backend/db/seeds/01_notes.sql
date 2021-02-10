2e8813a5858c9cfa86e6efb3b3974e93e78b9b2a
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-10 04:28:42 +0000

    created seed for notes table

---
+++ backend/db/seeds/01_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes(title,content)
+VALUES
+     ('JavaScript', 'JavaScript is a functional programming language'),
+     ('React','React is used for Frontend Development by making different components and we can test these components in isolation')
+    
