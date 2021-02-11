f242bcf94761b6e183f1908d77fb5eac537ec2f1
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-10 23:18:37 -0800

    used post/id request to axios to delete a note from database

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 import Note from './Note';
 import CreateArea from './CreateArea';
 import axios from 'axios';
-
+const {getIdFromNotes} = require('../helpers/getIdFromNotes');
 export default function App() {
   const [notes, setNotes] = useState([]);
   useEffect(()=> {
@@ -13,22 +13,28 @@ export default function App() {
       .then(res => {
           setNotes(res.data.notes);
       });
-  },[])
+  },[]   )
   function addNote(note) {
     axios
     .post('http://localhost:8000/api/notes',note)
     .then(res => {
-      console.log(res);
+      setNotes([...notes, note]);
     })
-    setNotes([...notes, note]);
+    
   }
-  function DeleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((note,index)=>{
-        return index !== id;
+  function DeleteNote(id,dbId) {
+    console.log(getIdFromNotes(notes,id));
+    axios
+    .post(`http://localhost:8000/api/notes/${dbId}`)
+    .then(res => {
+      setNotes(prevNotes => {
+        return prevNotes.filter((note,index)=>{
+          return index !== id;
+      })
+     
+      })
     })
    
-    })
    
   }
   return(
@@ -37,7 +43,7 @@ export default function App() {
       <CreateArea onAdd={addNote} notes={notes} setNotes={setNotes}/>
         {
         notes.map((note,i) => {
-          return( <Note onDelete={DeleteNote} key={i} id={i} title={note.title} content={note.content} />)
+          return( <Note onDelete={DeleteNote} key={i} id={i} dbId={note.id} title={note.title} content={note.content} />)
         })
         }
       <Footer />
