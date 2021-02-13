d4a28b7b180f26b91c2f602aa12bf9fae4c70c39
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-12 20:45:04 -0800

    removed useState and used setToken function to set token to sessionStorage and used getToken function to receive token

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,8 +2,17 @@ import react, { useState } from 'react';
 import Notes from './Notes';
 import Login from './login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token;
+}
 export default function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if (!token) {
     return <Login setToken={setToken} />
