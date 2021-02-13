e2ea5938504512ed14119acd5ce35ac3f33f468b
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-02-12 20:58:44 -0800

    imported useToken custom hook and destructured token and setToken

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,17 +2,12 @@ import react, { useState } from 'react';
 import Notes from './Notes';
 import Login from './login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
+import useToken from '../hooks/useToken'
 
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token;
-}
+
 export default function App() {
-  const token = getToken();
+  const {token,setToken} = useToken();
+  
 
   if (!token) {
     return <Login setToken={setToken} />
